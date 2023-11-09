3464a7f43a86f176b5cc799e4096c71e66c5b04a
GUILHERME DINIZ <gdiniztec@yahoo.com.br>
2023-11-09 20:02:02 -0300

    H2 database

--- backend/dscatalog/src/main/java/com/example/dscatalog/resources/CategoryResource.java
+++ backend/dscatalog/src/main/java/com/example/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.example.dscatalog.resources;
 
 import com.example.dscatalog.entities.Category;
+import com.example.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,11 @@ import java.util.List;
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
+    @Autowired
+    private CategoryService service;
     @GetMapping
     public ResponseEntity<List<Category>>findAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L,"Books"));
-        list.add(new Category(2L,"Eletronics"));
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
