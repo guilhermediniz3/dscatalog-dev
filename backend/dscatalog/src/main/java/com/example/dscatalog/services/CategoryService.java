bf22cd45dcf6b31fd42796907f8d21b28cad8a1d
GUILHERME DINIZ <gdiniztec@yahoo.com.br>
2023-11-09 21:41:08 -0300

    implementação da conversão de category para category dto usando for

--- backend/dscatalog/src/main/java/com/example/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/example/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.example.dscatalog.services;
 
+import com.example.dscatalog.dto.CategoryDTO;
 import com.example.dscatalog.entities.Category;
 import com.example.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,8 +17,15 @@ public class CategoryService {
 
 
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return repository.findAll();
+    public List<CategoryDTO> findAll() {
+      List<Category>list= repository.findAll();
+      List<CategoryDTO>  listDto = new ArrayList<>();
+      for(Category cat: list){
+         listDto.add(new CategoryDTO(cat));
+
+      }
+      return listDto;
+      }
 
     }
 
