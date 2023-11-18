d49c61e33ce889452efc6a05d4e21ecf8c74fba5
GUILHERME DINIZ <gdiniztec@yahoo.com.br>
2023-11-18 19:42:17 -0300

    Product insert , update e delete

--- backend/dscatalog/src/main/java/com/example/dscatalog/services/ProductService.java
+++ backend/dscatalog/src/main/java/com/example/dscatalog/services/ProductService.java
@@ -1,13 +1,15 @@
 package com.example.dscatalog.services;
 
 
+import com.example.dscatalog.dto.CategoryDTO;
 import com.example.dscatalog.dto.ProductDTO;
+import com.example.dscatalog.entities.Category;
 import com.example.dscatalog.entities.Product;
+import com.example.dscatalog.repositories.CategoryRepository;
 import com.example.dscatalog.repositories.ProductRepository;
 import com.example.dscatalog.services.exceptions.DatabaseException;
 import com.example.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -17,15 +19,19 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+
 @Service
 public class ProductService {
 
-@Autowired
-ProductRepository repository;
+    @Autowired
+    ProductRepository repository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     // Page já é um stream então foi removido do codigo junto do collectors.tolist
-    @Transactional(readOnly= true)
+    @Transactional(readOnly = true)
     public Page<ProductDTO> findAll(PageRequest pageRequest) {
-        Page<Product>list= repository.findAll(pageRequest);
+        Page<Product> list = repository.findAll(pageRequest);
         return list.map(product -> new ProductDTO(product));
 
 
@@ -33,32 +39,47 @@ ProductRepository repository;
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Optional<Product> obj= repository.findById(id);
-        Product entity = obj.orElseThrow(()->new ResourceNotFoundException("Produto não encontrada"));
+        Optional<Product> obj = repository.findById(id);
+        Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Produto não encontrado"));
         return new ProductDTO(entity, entity.getCategories());
 
     }
+
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
         Product entity = new Product();
-     //   entity.setName(dto.getName());
+        copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ProductDTO(entity);
 
     }
-    @Transactional
-    public ProductDTO update(ProductDTO dto, Long id) {
-        try{
-            Product entity = repository.getReferenceById(id);
-          //  entity.setName(dto.getName());
-            entity = repository.save(entity);
-            return new ProductDTO(entity);
-        }
-        catch (EntityNotFoundException e){
 
-            throw new ResourceNotFoundException("id"+ " " + id +" "+ "não encontrado");
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setDate(dto.getDate());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setPrice(dto.getPrice());
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category category = categoryRepository.getReferenceById(catDto.getId());
+            entity.getCategories().add(category);
         }
     }
+
+    @Transactional
+    public ProductDTO update(ProductDTO dto, Long id) {
+        try {
+            Product entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ProductDTO(entity);
+        } catch (EntityNotFoundException e) {
+
+            throw new ResourceNotFoundException("id" + " " + id + " " + "não encontrado");
+        }
+    }
+
     @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
         if (!repository.existsById(id)) {
@@ -66,8 +87,7 @@ ProductRepository repository;
         }
         try {
             repository.deleteById(id);
-        }
-        catch (DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Falha de integridade referencial");
         }
     }
